51c2e5fe2ea6b150ac90db9ec2861a980d59d9cf
= <chase.terry87@gmail.com>
2019-04-18 14:08:44 -0500

    Created a basic prototype of what the app will look like.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,188 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <h1 class="title has-text-centered">Idealist - Where great ideas come to light!</h1>
+    <div class="columns">
+        <div class="column is-half">
+            <div class="box">
+                <h2 class="subtitle">Submit your ideas!</h2>
+                <div class="field has-addons">
+                    <div class="control has-icons-left is-expanded">
+                        <input type="text" class="input is-medium" placeholder="This could be your best idea yet!">
+                        <span class="icon is-left"><i class="fas fa-lightbulb"></i></span>
+                    </div>
+                    <div class="control">
+                        <button class="button is-info is-medium">Submit</button>
+                    </div>
                 </div>
+            </div>
+            <div class="box">
+                <h2 class="subtitle">Cast your votes!</h2>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
+            </div>
+        </div>
+        <div class="column is-half">
+            <div class="box">
+                <h2 class="subtitle">Top ideas</h2>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-up"></i>
+                        </span>
+                        <span class="icon is-small">
+                            <i class="fa fa-arrow-down"></i>
+                        </span>
+                    </div>
+                </article>
             </div>
         </div>
     </div>
