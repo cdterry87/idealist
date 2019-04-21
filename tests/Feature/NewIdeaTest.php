673349b50454c30b7c1e31465f708345aa1c4173
Harden Wallace <hardentwallace@gmail.com>
2019-04-21 20:56:17 +0000

    Setup Idea Controller, Model, Factory, and Test.

---
+++ tests/Feature/NewIdeaTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class NewIdeaTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_idea_can_be_created()
+    {
+        
+    }
+}
